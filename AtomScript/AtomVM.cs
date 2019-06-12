54f2c349a29046006feb7bcdd15467f7a8090031
kongyatong <839339849@qq.com>
2019-06-12 13:39:26 +0800

    init project

---
+++ AtomScript/AtomVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AtomScript
+{
+    class AtomVM
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
